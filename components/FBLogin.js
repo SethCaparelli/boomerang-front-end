56cc92d7774170b39840f1dc2e1350bcc1527abf
Seth Caparelli <sethcaparelli@gmail.com>
2018-04-30 19:08:55 -0600

    post request for fblogin written

--- components/FBLogin.js
+++ components/FBLogin.js
@@ -4,13 +4,39 @@ import FBSDK, { AccessToken } from 'react-native-fbsdk'
 const { LoginButton } = FBSDK
 
 export default class FBLogin extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: {}
+    }
+  }
 
   initUser = (token) => {
     console.log("init")
     fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
     .then((response) => response.json())
-    .then((json) => {
-      console.log(json)
+    .then((user) => {
+      this.setState({
+        currentUser: user
+      })
+      fetch("http://localhost:3000/users")
+        .then(response => response.json())
+        .then(users => {
+          for(let i = 0; i <= users.length; i++) {
+            if(users[i].email === user.email) {
+              this.setState({
+                currentUser: users[i]
+              })
+            } else {
+              fetch("http://localhost:3000/users", {
+                method: "POST",
+                body: JSON({user})
+              })
+              .then(response => console.log(response))
+            }
+          }
+        })
+        .catch(error => console.log(error))
     })
     .catch(() => {
       reject('ERROR GETTING DATA FROM FACEBOOK')
